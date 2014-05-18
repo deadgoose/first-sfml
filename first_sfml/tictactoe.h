13eda5fa121a44da45cda4ad70428fdb1bc5537b
Ben Rainero <benrainero@Bens-Mac-Pro.local>
2014-05-18 17:59:22 -0400

    started header file for tic tac toe game and boxes

---
+++ first_sfml/tictactoe.h
@@ -0,0 +1,26 @@
+//
+//  tictactoe.h
+//  first_sfml
+//
+//  Created by Ben Rainero on 5/18/14.
+//
+//
+
+#ifndef first_sfml_tictactoe_h
+#define first_sfml_tictactoe_h
+
+class ticTacToe {
+public:
+    tictactoe::ticTacToeBox* array_boxes;
+    
+    int setBox(int x, int y, int player) {
+        
+        return 0;
+    }
+};
+
+class ticTacToeBox {
+    int owner;
+};
+
+#endif
